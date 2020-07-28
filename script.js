43be59e0915ddd34290a636fa5ad0a80318d5ed3
Lorenalgm <lorena@proesc.com>
2020-07-28 19:16:15 -0300

    Finalizada estilização do formulário 1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var start = document.querySelector('.start');
 var body = document.querySelector('.body-container');
 var form = document.querySelector('.form');
+var button1 = document.querySelector('.go_to_page2');
+var button2 = document.querySelector('.go_to_page3');
+var pag1 = document.querySelector('.form-pag1');
+var pag2 = document.querySelector('.form-pag2');
+var pag3 = document.querySelector('.form-pag3');
 
 function HandleForm(){
     form.style.display = 'block';
@@ -9,3 +14,18 @@ function HandleForm(){
 
 
 start.addEventListener("click", HandleForm);
+
+
+function HandlePaginate2(){
+    pag2.style.display = 'block';
+    pag1.style.display = 'none';
+}
+
+button1.addEventListener("click", HandlePaginate2);
+
+function HandlePaginate3(){
+    pag3.style.display = 'block';
+    pag2.style.display = 'none';
+}
+
+button2.addEventListener("click", HandlePaginate3);
